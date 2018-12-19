ada8c1b26d816d409678481106148fa93ab97b3c
Mr. EndLess <aryanmk.mk5@gmail.com>
2018-12-19 18:30:20 +0530

    Create RoundViewExtension.swift

---
+++ RoundViewExtension.swift
@@ -0,0 +1,25 @@
+
+// Make UIView Round and enable extra option like (BorderWidth,BorderColor,ViewRadius) in Interface Builder
+
+@IBDesignable class RoundView: UIView {
+    
+    @IBInspectable lazy var isRoundRectButton : Bool = false
+    
+    @IBInspectable public var cornerRadius : CGFloat = 0.0 {
+        didSet{
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    @IBInspectable public var borderColor : UIColor = UIColor.clear {
+        didSet {
+            self.layer.borderColor = borderColor.cgColor
+        }
+    }
+    
+    @IBInspectable public var borderWidth : CGFloat = 0.0 {
+        didSet {
+            self.layer.borderWidth = borderWidth
+        }
+    }
+}
