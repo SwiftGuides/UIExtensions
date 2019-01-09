8da645e3bbed826beaebb5198d25b895f876c46a
Mr. EndLess <aryanmk.mk5@gmail.com>
2019-01-09 13:21:57 +0530

    Create ProgressBarExtension.swift

---
+++ ProgressBarExtension.swift
@@ -0,0 +1,16 @@
+//Extension For Adding extra Features to ProgressView
+extension UIProgressView {
+    
+    @IBInspectable var barHeight : CGFloat {
+        get {
+            return transform.d * 2.0
+        }
+        set {
+            // 2.0 Refers to the default height of 2
+            let heightScale = newValue / 2.0
+            let c = center
+            transform = CGAffineTransform(scaleX: 1.0, y: heightScale)
+            center = c
+        }
+    }
+}
