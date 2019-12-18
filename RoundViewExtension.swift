162866e03580064797e24e52ced964c60c8c5856
Mr. EndLess <aryanmk.mk5@gmail.com>
2019-12-18 18:10:13 +0530

    update  roundview with more Porperties

--- RoundViewExtension.swift
+++ RoundViewExtension.swift
@@ -3,6 +3,7 @@
 
 @IBDesignable class RoundView: UIView {
     
+    
     @IBInspectable lazy var isRoundRectButton : Bool = false
     
     @IBInspectable public var cornerRadius : CGFloat = 0.0 {
@@ -20,6 +21,53 @@
     @IBInspectable public var borderWidth : CGFloat = 0.0 {
         didSet {
             self.layer.borderWidth = borderWidth
+        }
+    }
+    
+    @IBInspectable
+    var shadowRadius: CGFloat {
+        get {
+            return layer.shadowRadius
+        }
+        set {
+            layer.shadowRadius = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOpacity: Float {
+        get {
+            return layer.shadowOpacity
+        }
+        set {
+            layer.shadowOpacity = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowOffset: CGSize {
+        get {
+            return layer.shadowOffset
+        }
+        set {
+            layer.shadowOffset = newValue
+        }
+    }
+    
+    @IBInspectable
+    var shadowColor: UIColor? {
+        get {
+            if let color = layer.shadowColor {
+                return UIColor(cgColor: color)
+            }
+            return nil
+        }
+        set {
+            if let color = newValue {
+                layer.shadowColor = color.cgColor
+            } else {
+                layer.shadowColor = nil
+            }
         }
     }
 }
